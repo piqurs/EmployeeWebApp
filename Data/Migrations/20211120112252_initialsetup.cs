c9ab137b2d746b0eea7a56d4683d55b375f4cd20
Anfiband <Anfiband@DESKTOP-MTA4QSR>
2021-11-20 18:52:16 +0700

    Add project files.

---
+++ Data/Migrations/20211120112252_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Employees = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true),
+                    Jabatan = table.Column<string>(nullable: true),
+                    Fasilitas = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Employees { get; set; }
+        public string Department { get; set; }
+        public string Jabatan { get; set; }
+        public string Fasilitas { get; set; }
+
+        public Employee()
+        {
+
+        }
+    }
+}
